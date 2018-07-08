41340fed9e3ed053e41f294e574287786a9c9df9
Justin Rovang <rovangju@users.noreply.github.com>
2018-07-07 23:28:13 -0500

    Update no-firewalld.sh

--- no-firewalld.sh
+++ no-firewalld.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+sudo yum install iptables-services
 sudo iptables-save > /etc/sysconfig/iptables-config
 sudo systemctl stop firewalld && sudo systemctl start iptables
 sudo firewall-cmd --state
 sudo systemctl disable firewalld
 sudo systemctl mask firewalld
 sudo systemctl enable iptables
+
+sudo cat /etc/sysconfig/iptables
