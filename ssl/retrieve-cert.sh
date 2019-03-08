4267e60ad80b8639db4a9112473ec142bd5664c1
Justin Rovang <rovangju@users.noreply.github.com>
2019-03-08 15:09:21 -0600

    Update retrieve-cert.sh

--- ssl/retrieve-cert.sh
+++ ssl/retrieve-cert.sh
@@ -5,6 +5,12 @@
 REMHOST=$1
 REMPORT=${2:-443}
 
+if [ ! $1 ]; then
+	echo "usage: retrieve-cert.sh remote.host.name [port]"
+	exit 1
+fi
+
 echo |\
-openssl s_client -connect ${REMHOST}:${REMPORT} 2>&1 |\
+openssl s_client -showcerts -servername ${REMHOST} -connect ${REMHOST}:${REMPORT} 2>&1 |\
+openssl x509 -text |\
 sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p'
