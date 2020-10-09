7dbfc763417cd3b67936f57f456aa9502943b1eb
Justin Rovang <rovangju@itnobody.com>
2020-10-08 22:35:14 -0500

    Re-organization

--- comforts/iterm2/itermhandler.sh
+++ comforts/iterm2/itermhandler.sh
@@ -4,7 +4,6 @@ line_number="${2:-}"
 if [[ -f "$file" ]]; then
     printf "vim ${line_number:++}${line_number} %q\n" "$file"
 else
-    printf "cd %q\n" "$file"
+    printf "cd %q\n" "${file}"
+    printf "ls -lah\n"	
 fi
-
-## Set up as a corprocess, /path/to/itermhanlder.sh \1 \2
---
+++ devstuff/brew-sqlserver.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+
+brew tap microsoft/mssql-release https://github.com/Microsoft/homebrew-mssql-release
+brew update
+HOMEBREW_NO_ENV_FILTERING=1 ACCEPT_EULA=Y brew install msodbcsql17 mssql-tools
+
