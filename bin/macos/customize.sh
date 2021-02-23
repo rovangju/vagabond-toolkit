76eaae939b11040dc8675f7203a69f9d1f38984e
Justin Rovang <rovangju@itnobody.com>
2021-02-23 00:54:38 -0600

    Kill the stupid safari nag

--- bin/macos/customize.sh
+++ bin/macos/customize.sh
@@ -10,3 +10,7 @@ defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
 #finder list view
 defaults write com.apple.Finder FXPreferredViewStyle Nlsv
 defaults write com.apple.finder AppleShowAllFiles NO 
+
+defaults write com.apple.coreservices.uiagent CSUIHasSafariBeenLaunched -bool YES
+defaults write com.apple.coreservices.uiagent CSUIRecommendSafariNextNotificationDate -date 2050-01-01T00:00:00Z
+defaults write com.apple.coreservices.uiagent CSUILastOSVersionWhereSafariRecommendationWasMade -float 10.99
