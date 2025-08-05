8658da1d7bbecd0c99a9994dbfd7f824b478c91c
Justin Rovang <rovangju@itnobody.com>
2025-08-05 10:48:47 -0500

    tweak for WSL hell

--- bin/linux/os.zsh
+++ bin/linux/os.zsh
@@ -9,9 +9,11 @@ if ! command -v pbcopy > /dev/null 2>&1; then
 		alias pbcopy='clipcopy'
 		alias pbpaste='clippaste'	
 	fi
+	if command -v clip.exe > /dev/null 2>&1; then
+		alias pbcopy='clip.exe'
+		alias pbpaste="powershell.exe Get-Clipboard | tr -d '\r'"
  	else if command -v xclip > /dev/null 2>&1; then
 		alias pbcopy='xclip -selection clipboard'
 		alias pbpaste='xclip -selection clipboard -o'
 	fi
-	
 fi
